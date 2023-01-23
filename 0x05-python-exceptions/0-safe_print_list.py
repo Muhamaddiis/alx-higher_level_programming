e7a3c556800b7b806b411b7982fffbd760981be9
unknown <khalitiman17@gmail.com>
2023-01-23 13:22:35 +0300

    testing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,9 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
-            total += 1
         except IndexError:
             break
+        else:
+            total += 1
     print()
     return (total)
