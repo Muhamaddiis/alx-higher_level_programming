e3b5f3d48b73f450c8811103e1c4819bd8c84556
unknown <khalitiman17@gmail.com>
2023-04-12 15:29:45 +0300

    testing

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
-let args = procces.argv.slice(2);
+#!/usr/bin/node
 
-if (args.length === 0) {
+let arg = procces.argv.slice(2);
+
+if (arg.length === 0) {
     console.log('No argument');
-} else if (args.length === 1) {
+} else if (arg.length === 1) {
     console.log("Argument found");
 } else {
     console.log("Arguments found");
