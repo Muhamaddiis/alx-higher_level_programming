527df01611a3612a38b03b33821e3515e20120f9
unknown <khalitiman17@gmail.com>
2023-04-13 17:11:24 +0300

    warmup

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-
-let arg = procces.argv.slice(2);
+const arg = process.argv.slice(2);
 
 if (arg.length === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (arg.length === 1) {
-    console.log("Argument found");
+  console.log('Argument found');
 } else {
-    console.log("Arguments found");
+  console.log('Arguments found');
 }
