9bd256676c9d1acf14230c9726d64be498e5e29c
Muhamaddiis <khalitiman17@gmail.com>
2023-08-31 13:01:13 -0700

    done

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes url sends POST request to passed url & display body of the response
-curl -sX POST -d email=test@gmail.com -d subject="I will always be here for PLD" "$1"
+#takes in a URL as an argument, sends a GET request to the URL,and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
