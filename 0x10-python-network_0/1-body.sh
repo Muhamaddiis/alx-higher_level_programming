d0e3fc3a5f9bfc44521cf40e5723ec59c85f821d
Muhamaddiis <khalitiman17@gmail.com>
2023-08-31 12:54:30 -0700

    done

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#getting response body of a given URL for 200 status code responses.
+curl -sL "$1"
---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#takes in a url and displays HTTP methods the server will accept
+curl -sI "$1" | grep "Allow:" | cut -d " " -f2-
+
+
+
+
