7aff8532a6c5ce9fe98a932ffdb8aaa5cb017f12
Muhamaddiis <khalitiman17@gmail.com>
2023-08-31 12:28:08 -0700

    fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 #a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-#The size must be displayed in bytes
-#You have to use curl
-
 curl -s "$1" | wc -c
